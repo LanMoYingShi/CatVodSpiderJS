0a65dd7b70c6bbd8b3be9a2e2c305e6ff10d72e5
catvod <>
2022-12-05 10:08:07 +0800

    new version.

---
+++ demo.js
@@ -0,0 +1,34 @@
+function init(ext) {
+}
+
+function home(filter) {
+}
+
+// Deprecated
+function homeVod(params) {
+}
+
+function category(tid, pg, filter, extend) {
+}
+
+function detail(id) {
+}
+
+function play(flag, id, flags) {
+}
+
+function search(wd, quick) {
+}
+
+// 导出函数对象
+export function __jsEvalReturn() {
+    return {
+        init: init,
+        home: home,
+        homeVod: homeVod,
+        category: category,
+        detail: detail,
+        play: play,
+        search: search,
+    };
+}
